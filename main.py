c32c2d85c514df11f16c16fccf1e56a4456bb90c
Bobteen <bobteen7@gmail.com>
2022-10-11 16:05:37 +0200

    sleep - паузы между get запросами маскируем заголовки запроса - headers

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
-for count in range(1, 8):
+list_card_url = []
+
+headers = {"User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"}
+
+for count in range(1, 8): # получаем карточки товара
+    sleep(3)
     url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, 'lxml')  # получаем обработанный текст страницы с сайта
 
     data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
 
     for i in data:
+        card_url = 'https://scrapingclub.com' + i.find('a').get('href')
+        list_card_url.append(card_url)
 
-        name = i.find('h4', class_='card-title').text.replace('\n', '')
-        price = i.find('h5').text
-        url_img = 'https://scrapingclub.com' + i.find('img', class_='card-img-top img-fluid').get('src')
-
-        print(name + '\n' + price + '\n' + url_img + '\n\n')
