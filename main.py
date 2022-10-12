52639a84a4c9fd91cc8673fd7e7f5e7acb6adf01
Bobteen <bobteen7@gmail.com>
2022-10-12 21:41:46 +0200

    меняем список list_card_url на парсер-генератор(функция get_url)

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = []
-
 headers = {"User-Agent":
                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"}
 
-for count in range(1, 8):  # получаем карточки товара
-    sleep(3)
-    url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    response = requests.get(url, headers=headers)  # отправляем запрос на сайт и получаем html код первой страницы с каталогом товаров
+def get_url():
+    for count in range(1, 8):  # получаем карточки товара
+        url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    soup = BeautifulSoup(response.text, 'lxml')  # разбираем эту страницу парсером
+        response = requests.get(url, headers=headers)  # отправляем запрос на сайт и получаем html код первой страницы с каталогом товаров
 
-    # по тегу находим html код карточки товара
-    # получаем список из всех карт
-    data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+        soup = BeautifulSoup(response.text, 'lxml')  # разбираем эту страницу парсером
 
-    # получаем ссылки на каждую карту товара и добавляем в список
-    for i in data:
-        card_url = 'https://scrapingclub.com' + i.find('a').get('href')
-        list_card_url.append(card_url)
+        # по тегу находим html код карточки товара
+        # получаем список из всех карт
+        data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+
+        # получаем ссылки на каждую карту товара
+        for i in data:
+            card_url = 'https://scrapingclub.com' + i.find('a').get('href')
+            yield card_url
+
 
 # проходим по списку из ссылок
-for card_url in list_card_url:
+for card_url in get_url():
     response = requests.get(card_url, headers=headers)
-
+    sleep(3)
     soup = BeautifulSoup(response.text, 'lxml')
 
     data = soup.find('div', class_='card mt-4 my-4')
